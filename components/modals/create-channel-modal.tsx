8c2ad866cf26199be49a1da052a57fd31c139b48
Weiliang Chen <liang2dd@gmail.com>
2023-11-27 18:14:25 +1100

    server channel / member list

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -34,6 +34,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { ChannelType } from "@prisma/client";
+import { useEffect } from "react";
 
 const formSchema = z.object({
   name: z
@@ -48,20 +49,29 @@ const formSchema = z.object({
 });
 
 export const CreateChannelModal = () => {
-  const { isOpen, onClose, type } = useModal();
+  const { isOpen, onClose, type, data } = useModal();
 
   const params = useParams();
   const router = useRouter();
 
   const isModalOpen = isOpen && type === "createChannel";
+  const { channelType } = data;
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      type: ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     },
   });
+
+  useEffect(() => {
+    if (channelType) {
+      form.setValue("type", channelType);
+    } else {
+      form.setValue("type", ChannelType.TEXT)
+    }
+  }, [channelType, form]);
 
   const isLoading = form.formState.isSubmitting;
 
