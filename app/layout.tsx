983917ed12d944cba68973d1e0ca158882223430
Weiliang Chen <liang2dd@gmail.com>
2023-11-30 19:43:02 +1100

    setup IO socket

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,12 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { cn } from "@/lib/utils";
 
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-prodvider";
-
-import { cn } from "@/lib/utils";
-
+import { SocketProvide } from "@/components/providers/socket-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -31,8 +30,10 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
-            <ModalProvider />
-            {children}
+            <SocketProvide>
+              <ModalProvider />
+              {children}
+            </SocketProvide>
           </ThemeProvider>
         </body>
       </html>
