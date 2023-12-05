ca7767bbba99717bdb796f26a11806d4b7ea738a
Weiliang Chen <liang2dd@gmail.com>
2023-12-05 18:28:45 +1100

    2nd deploy

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -1,7 +1,61 @@
+import { NextResponse } from "next/server";
+import { MemberRole } from "@prisma/client";
+
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { MemberRole } from "@prisma/client";
-import { NextResponse } from "next/server";
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { channelId: string } }
+) {
+  try {
+    const profile = await currentProfile();
+    const { searchParams } = new URL(req.url);
+
+    const serverId = searchParams.get("serverId");
+
+    if (!profile) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!serverId) {
+      return new NextResponse("Server ID missing", { status: 400 });
+    }
+
+    if (!params.channelId) {
+      return new NextResponse("Channel ID missing", { status: 400 });
+    }
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            }
+          }
+        }
+      },
+      data: {
+        channels: {
+          delete: {
+            id: params.channelId,
+            name: {
+              not: "general",
+            }
+          }
+        }
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[CHANNEL_ID_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
 
 export async function PATCH(
   req: Request,
@@ -27,7 +81,7 @@ export async function PATCH(
     }
 
     if (name === "general") {
-      return new NextResponse("Name cannot be general", { status: 400 });
+      return new NextResponse("Name cannot be 'general'", { status: 400 });
     }
 
     const server = await db.server.update({
@@ -38,15 +92,15 @@ export async function PATCH(
             profileId: profile.id,
             role: {
               in: [MemberRole.ADMIN, MemberRole.MODERATOR],
-            },
-          },
-        },
+            }
+          }
+        }
       },
       data: {
         channels: {
           update: {
             where: {
-              id: params?.channelId,
+              id: params.channelId,
               NOT: {
                 name: "general",
               },
@@ -54,69 +108,15 @@ export async function PATCH(
             data: {
               name,
               type,
-            },
-          },
-        },
-      },
+            }
+          }
+        }
+      }
     });
 
     return NextResponse.json(server);
   } catch (error) {
-    console.log("[CHANNEL_ID_PATCH", error);
-    return new NextResponse("Internal Error", { status: 500 });
-  }
-}
-
-export async function DELETE(
-  req: Request,
-  { params }: { params: { channelId: string } }
-) {
-  try {
-    const profile = await currentProfile();
-
-    const { searchParams } = new URL(req.url);
-
-    const serverId = searchParams.get("serverId");
-
-    if (!profile) {
-      return new NextResponse("Unauthorized", { status: 401 });
-    }
-
-    if (!serverId) {
-      return new NextResponse("Server ID missing", { status: 400 });
-    }
-
-    if (!params.channelId) {
-      return new NextResponse("Channel ID missing", { status: 400 });
-    }
-
-    const server = await db.server.update({
-      where: {
-        id: serverId,
-        members: {
-          some: {
-            profileId: profile.id,
-            role: {
-              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
-            },
-          },
-        },
-      },
-      data: {
-        channels: {
-          delete: {
-            id: params.channelId,
-            name: {
-              not: "general",
-            },
-          },
-        },
-      },
-    });
-
-    return NextResponse.json(server);
-  } catch (error) {
-    console.log("[CHANNEL_ID_DELETE", error);
+    console.log("[CHANNEL_ID_PATCH]", error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 }
